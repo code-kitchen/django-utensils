c1b88c14b592217296117157c6ac62dad0ac057d
Ben Tappin <ben@mrben.co.uk>
2013-11-28 22:59:39 +0000

    Update views.py comments.

--- utensils/views.py
+++ utensils/views.py
@@ -5,7 +5,27 @@ from .viewmixins import OrderByMixin, PaginateMixin, SearchFormMixin
 
 
 class BaseListView(PaginateMixin, OrderByMixin, SearchFormMixin, ListView):
-
+    """
+    Defines a base list view that supports, pagination, ordering and basic search.
+    
+    Supports a filter description that can be used in templates:
+    
+        class ActiveCustomerList(BaseListView):
+            filter_description = u"Active"
+            queryset = Artist.active.all()
+    
+    
+        {% block content %}
+          <h2>
+            {% if filter_description %}
+              {{ filter_description|title }} customers
+            {% else %}
+              Customers
+            {% endif %}
+          </h2>
+          <! -- more template -->
+        {% end block %}
+    """
     def get_filter_description(self):
         if hasattr(self, 'filter_description'):
             return self.filter_description
