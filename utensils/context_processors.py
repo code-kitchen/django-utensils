d0f0f8d2c27a97c3108e343b86511f656f23be91
Ben Tappin <ben@mrben.co.uk>
2013-11-28 22:38:06 +0000

    Update context_processors.py comments.

--- utensils/context_processors.py
+++ utensils/context_processors.py
@@ -3,6 +3,12 @@ from django.conf import settings
 
 
 def pagination(request):
+    """
+    Used by the `pagination` template tag.
+    
+    Example settings config:
+      PAGINATION_PAGE_SIZES = [20, 50, 100]
+    """
     return {
         'pagination_page_sizes': settings.PAGINATION_PAGE_SIZES,
     }
