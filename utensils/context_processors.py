4069d3a9b78df3d2f9e2b9dbd281d400e9ae4e29
Ben Tappin <ben@mrben.co.uk>
2013-11-28 22:40:58 +0000

    Update context_processors.py with comments and a page sizes default.

--- utensils/context_processors.py
+++ utensils/context_processors.py
@@ -4,11 +4,12 @@ from django.conf import settings
 
 def pagination(request):
     """
-    Used by the `pagination` template tag.
+    Provides a list of pagination sizes to the `pagination` template tag.
     
     Example settings config:
       PAGINATION_PAGE_SIZES = [20, 50, 100]
     """
+    page_sizes = getattr(settings, 'PAGINATION_PAGE_SIZES', [20, 50, 100])
     return {
-        'pagination_page_sizes': settings.PAGINATION_PAGE_SIZES,
+        'pagination_page_sizes': page_sizes,
     }
