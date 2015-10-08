329d5454e0dc60c474ad4f91c836919096e93e03
Ben Tappin <ben@mrben.co.uk>
2015-10-08 14:37:12 +0100

    Bump version and update setup.py.

--- setup.py
+++ setup.py
@@ -19,6 +19,15 @@ with open('LICENSE') as f:
     license = f.read()
 
 
+requirements = [
+    'django-bootstrap3',
+    'django-braces<1.9.0',
+    'django-countries-plus>=0.1.5',
+    'django-storages>=1.1',
+    'boto>=2.13.3',
+]
+
+
 setup(
     name='django-utensils',
     version=utensils.__version__,
@@ -28,13 +37,7 @@ setup(
     author='Ben Tappin',
     author_email='ben@mrben.co.uk',
     license=license,
-    install_requires=[
-        'django-bootstrap3',
-        'django-braces<1.9.0',
-        'django-countries-plus>=0.1.5',
-        'django-storages>=1.1',
-        'boto>=2.13.3',
-    ],
+    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -42,8 +45,14 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Natural Language :: English',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
         'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     )
 )
